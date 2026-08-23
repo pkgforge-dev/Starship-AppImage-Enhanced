d6a340ba35f60adce6fc731a6b0e46d12b92258b
Samuel <36420837+Samueru-sama@users.noreply.github.com>
2026-08-23 17:06:30 -0400

    fix o2r gen, build less often (#1)
    * fix o2r gen
    
    * build less often

--- README.md
+++ README.md
@@ -17,8 +17,6 @@
 
 </div>
 
-### ⚠️ Need to generate `sf64.o2r` using official [AppImage](https://github.com/HarbourMasters/Starship/releases/latest) and place in `~/.local/share/ship/` ⚠️
-
 ---
 
 AppImage made using [quick-sharun](https://github.com/pkgforge-dev/Anylinux-AppImages/blob/main/useful-tools/quick-sharun.sh), which makes it extremely easy to turn any binary into a portable package reliably without using containers or similar tricks.
--- get-dependencies.sh
+++ get-dependencies.sh
@@ -7,47 +7,44 @@ ARCH=$(uname -m)
 echo "Installing package dependencies..."
 echo "---------------------------------------------------------------"
 pacman -Syu --noconfirm \
-    cmake         \
-    fmt           \
-    libdecor      \
-    libzip        \
-    ninja         \
-    nlohmann-json \
-    sdl2          \
-    spdlog        \
-    tinyxml2
+	cmake         \
+	fmt           \
+	libzip        \
+	ninja         \
+	nlohmann-json \
+	sdl2          \
+	spdlog        \
+	tinyxml2
 
 echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
-get-debloated-pkgs --add-common --prefer-nano
+get-debloated-pkgs --add-common --prefer-nano libdecor-mini
 
+# Comment this out if you need an AUR package
 make-aur-package zenity-rs-bin
 
+# If the application needs to be manually built that has to be done down here
 echo "Making stable build of Starship..."
 echo "---------------------------------------------------------------"
-REPO="https://github.com/HarbourMasters/Starship"
-TAG="$(git ls-remote --tags --sort="v:refname" "$REPO" | tail -n1 | sed 's/.*\///; s/\^{}//')"
-git clone --branch "$TAG" --single-branch --recursive --depth 1 "$REPO" ./Starship
-echo "${TAG#v}" > ~/version
+REPO=https://github.com/HarbourMasters/Starship
+TAG=$(git ls-remote --tags --refs --sort=-v:refname "$REPO" | awk -F/ '{print $NF; exit}')
+git clone --branch "$TAG" --single-branch --recursive --depth 1 "$REPO" ./Starship && (
+	cd ./Starship
+
+	patch -Np1 -i ../patches/starship-stack-underflow-guard.patch
+	patch -Np1 -i ../patches/starship-non-portable-fix.patch
+	patch -Np1 -i ../patches/torch-src-dest-paths.patch
+	sed -i 's/-mfpu=neon/-mcpu=native/' CMakeLists.txt
+
+	cmake ./ -Bbuild -GNinja -DCMAKE_BUILD_TYPE=Release -DCMAKE_POSITION_INDEPENDENT_CODE=ON
+	cmake --build build --config Release
+	cmake --build build --config Release --target GeneratePortO2R
+	echo "${TAG#v}" > ~/version
+)
 
 mkdir -p ./AppDir/bin
-cd ./Starship
-patch -Np1 -i ../starship-stack-underflow-guard.patch
-#patch -Np1 -i ../starship-non-portable-fix.patch
-sed -i 's/-mfpu=neon/-mcpu=native/' CMakeLists.txt
-
-cmake . \
-    -Bbuild \
-    -GNinja \
-    -DCMAKE_POSITION_INDEPENDENT_CODE=ON
-
-cmake --build build --config Release
-cmake --build build --config Release --target GeneratePortO2R
-
-mv -v build/assets ../AppDir/bin
-mv -v build/Starship ../AppDir/bin
-mv -v build/config.yml ../AppDir/bin
-mv -v build/starship.o2r ../AppDir/bin
-wget -O ../AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
-cp -v logo.png ../AppDir/.DirIcon
-mv -v logo.png ../AppDir/starship.png
+for a in assets Starship config.yml starship.o2r; do
+	mv -v ./Starship/build/"$a" ./AppDir/bin
+done
+wget -O ./AppDir/bin/gamecontrollerdb.txt https://raw.githubusercontent.com/mdqinc/SDL_GameControllerDB/master/gamecontrollerdb.txt
+mv -v ./Starship/logo.png ./AppDir/starship.png
